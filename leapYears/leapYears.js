ffffa033714c486129efaf7a0ffc7823f09cdbf3
henry kirya <henry.kirya@gmail.com>
2019-06-20 09:23:05 +0300

    completed leap years

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(yy) {
+	if ((yy%400==0) || ((yy%4 == 0) && (yy%100 != 0))) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 module.exports = leapYears
