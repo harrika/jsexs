0551f7fb503449e59cdbb5cbd8d04022d4633903
henry kirya <henry.kirya@gmail.com>
2019-06-25 14:57:43 +0300

    added get oldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-let findTheOldest = function() {
+let findTheOldest = function(ppl) {
 
+	const ordered = ppl.sort(function(a,b) {
+		if (a.yearOfDeath == null) {
+			a.yearOfDeath = new Date().getFullYear();
+		}
+		if (b.yearOfDeath == null) {
+			b.yearOfDeath = new Date().getFullYear();
+		}
+
+	    return ((a.yearOfDeath - a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth)) 
+	});
+
+	return ordered[0];
 }
 
 module.exports = findTheOldest
+
