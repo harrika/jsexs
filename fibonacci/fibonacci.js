624abd5583c642786be1e35996943825ba1c9226
henry kirya <henry.kirya@gmail.com>
2019-06-25 12:47:48 +0300

    added fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+	if (n<0) {
+		return 'OOPS';
+	}
+	if (n==1 || n==2) {
+		return 1;
+	} else {
+		return fibonacci(n-1)+fibonacci(n-2)
+	}
 
 }
 
 module.exports = fibonacci
+
+// xx = fibonacci(6);
+// console.log(xx);
