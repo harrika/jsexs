be6557ba0233e5243a48de7c4f30f3f64e8faaf2
henry kirya <henry.kirya@gmail.com>
2019-06-25 13:54:05 +0300

    added get titles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,26 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bb) {
+	titles = [];
+	for (let i in bb) {
+		ray = bb[i];
+		singleTitle = ray['title'];
+		titles.push(singleTitle);
+	}
+	return titles
 }
-
+ 
 module.exports = getTheTitles;
+
+
+// const books = [
+//   {
+//     title: 'Book',
+//     author: 'Name'
+//   },
+//   {
+//     title: 'Book2',
+//     author: 'Name2'
+//   }
+// ]
+
+// console.log(getTheTitles(books));
+
